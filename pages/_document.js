3e0fbc091197dfda9714e64d54b4fb5db4fc23cd
Masum Billah <masumwen07@gmail.com>
2023-02-17 02:36:30 +0600

    rmv man js

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="manifest" data-backend-url={`${process.env.BASE_URL}`} data-frontend-url={`${process.env.FRONTEND_URL}`} id="mainifest-json" />
-        <script src="/mainifest.js" />
       </Head>
       <body >
         <link rel="stylesheet" href={`${process.env.BASE_URL}css/dynamic-color.css`} />
